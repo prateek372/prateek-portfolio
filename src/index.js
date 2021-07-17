3e2ef9290442617d14b0d1f69c50a991fb538b74
bot-py-372 <63001902+bot-py-372@users.noreply.github.com>
2021-07-17 03:32:22 -0400

    corrected image src
    corrected image src

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   // <React.StrictMode>
@@ -11,5 +10,3 @@ ReactDOM.render(
   // </React.StrictMode>
   document.getElementById('root')
 )
-
-serviceWorker.unregister()
